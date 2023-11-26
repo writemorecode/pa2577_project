988c96b791b6b5ee50181f5506bae54a7953c27f
writemorecode <gustav.ae.karlsson@gmail.com>
2023-11-26 17:45:49 +0100

    Image uploading now works

--- main.go
+++ main.go
@@ -1,16 +1,49 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+	"path/filepath"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
-func hello(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "hello!\n")
+func indexHandler(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.tmpl", gin.H{
+		"title": "Image Sharing Service",
+	})
+}
+
+func imageUploadHandler(c *gin.Context) {
+	file, err := c.FormFile("image")
+	if err != nil {
+		c.String(http.StatusBadRequest, "get form error: %s", err.Error())
+		return
+	}
+
+	filename := filepath.Base(file.Filename)
+	fileExtension := filepath.Ext(filename)
+	uuidFilename := uuid.New().String() + fileExtension
+	uploadPath := filepath.Join("uploads", uuidFilename)
+
+	if err := c.SaveUploadedFile(file, uploadPath); err != nil {
+		c.String(http.StatusBadRequest, "upload form error: %s", err.Error())
+		return
+	}
+	c.Redirect(http.StatusFound, uploadPath)
 }
 
 func main() {
-	http.HandleFunc("/hello", hello)
+	r := gin.Default()
 
-	http.ListenAndServe(":8080", nil)
+	r.Static("/uploads", "./uploads")
+
+	r.LoadHTMLGlob("templates/*")
+
+	// Max uploaded image size 8 MiB
+	r.MaxMultipartMemory = 8 << 20
+
+	r.GET("/", indexHandler)
+	r.POST("/upload", imageUploadHandler)
+	r.Run(":8080")
 }
